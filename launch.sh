0332a016995692431a8cc3428dc2accc13322ffb
lemorse <arthurr.caron@gmail.com>
2022-02-27 22:58:01 +0100

    :hammer: - Polybar configured with 3 monitors.

--- launch.sh
+++ launch.sh
@@ -9,20 +9,32 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bars
 if type "xrandr"; then
 
-	monitors=`xrandr --listmonitors | tail -n +2 | awk -F'\\ \\ ' '{print $NF}'`
-	len_monitors=`xrandr --listmonitors | head -n 1 | awk -F'\\ ' '{print $NF}'`
+	monitors=`xrandr --listactivemonitors | tail -n +2 | awk -F'\\ \\ ' '{print $NF}'`
+	len_monitors=`xrandr --listactivemonitors | head -n 1 | awk -F'\\ ' '{print $NF}'`
 
 	echo "${len_monitors} monitor(s) detected"
 
 	if [ $((len_monitors)) -eq 2 ]; then
-		monitor_1=`xrandr --listmonitors | tail -n +2 | awk -F'\\ \\ ' '{print $NF}' | head -1 | tail -1`
-		monitor_2=`xrandr --listmonitors | tail -n +2 | awk -F'\\ \\ ' '{print $NF}' | head -2 | tail -1`
+		monitor_1=`xrandr --listactivemonitors | tail -n +2 | awk -F'\\ \\ ' '{print $NF}' | head -1 | tail -1`
+		monitor_2=`xrandr --listactivemonitors | tail -n +2 | awk -F'\\ \\ ' '{print $NF}' | head -2 | tail -1`
 		MONITOR=$monitor_2 polybar -c $HOME/.config/polybar/bars-config nord-top &
-		echo "The top bar has been launched on the second monitor."
+		echo "The top bar has been launched on the external monitor."
 		MONITOR=$monitor_1 polybar -c $HOME/.config/polybar/bars-config nord-down &
-		echo "The down bar has been launched on the primary monitor."
+		echo "The down bar has been launched on the embedded monitor."
 		MONITOR=$monitor_1 polybar -c $HOME/.config/polybar/bars-config nord-top-2 &
-		echo "The second top bar has been launched on the primary monitor."
+		echo "The second top bar has been launched on the embedded monitor."
+	elif [ $((len_monitors)) -eq 3 ]; then
+		monitor_1=`xrandr --listactivemonitors | tail -n +2 | awk -F'\\ \\ ' '{print $NF}' | head -1 | tail -1`
+		monitor_2=`xrandr --listactivemonitors | tail -n +2 | awk -F'\\ \\ ' '{print $NF}' | head -2 | tail -1`
+		monitor_3=`xrandr --listactivemonitors | tail -n +2 | awk -F'\\ \\ ' '{print $NF}' | head -3 | tail -1`
+		MONITOR=$monitor_1 polybar -c $HOME/.config/polybar/bars-config nord-top &
+		echo "The top bar has been launched on the second external monitor."
+		MONITOR=$monitor_1 polybar -c $HOME/.config/polybar/bars-config nord-down &
+		echo "The down bar has been launched on the second external monitor."
+		MONITOR=$monitor_2 polybar -c $HOME/.config/polybar/bars-config nord-top-2 &
+		echo "The second top bar has been launched on the embedded monitor."
+		MONITOR=$monitor_3 polybar -c $HOME/.config/polybar/bars-config nord-top-2 &
+		echo "The second top bar has been launched on the first external monitor."
 	else
 		for m in $monitors; do
 			MONITOR=$m polybar -c $HOME/.config/polybar/bars-config nord-top &
